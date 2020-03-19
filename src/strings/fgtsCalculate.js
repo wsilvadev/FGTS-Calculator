ae0ebef45ca579f85e78634be813fda67041a864
Willian <wsilvadev@gmail.com>
2020-03-18 21:19:42 -0300

    feat(strings): create strings

---
+++ src/strings/fgtsCalculate.js
@@ -0,0 +1,11 @@
+const Fgts = {
+  salary: 'Gross salary',
+  real: 'R$',
+  fgtsValue: ' Percentage of FGTS ',
+  percent: '%',
+  cashoutAvailable: 'Cash Out Available...',
+  tapYourMoney: 'Enter the value below',
+  header: 'Fgts Calculator',
+};
+
+export default Fgts;
